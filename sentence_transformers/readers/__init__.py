9db59b7eb90019cc70088cd2c3a8fe008403ac70
Dennis Aumiller <dennis.aumiller@gmail.com>
2020-03-13 17:24:33 +0100

    Adding AGBDataReader to index.

--- sentence_transformers/readers/__init__.py
+++ sentence_transformers/readers/__init__.py
@@ -1,5 +1,6 @@
 from .InputExample import InputExample
 from .LabelSentenceReader import LabelSentenceReader
+from .AGBDataReader import AGBDataReader
 from .NLIDataReader import NLIDataReader
 from .STSDataReader import STSDataReader
 from .TripletReader import TripletReader
