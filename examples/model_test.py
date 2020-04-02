4c71cd675ef8995352c1e20b13741887a51f8741
Dennis Aumiller <dennis.aumiller@gmx.de>
2020-04-02 17:38:07 +0200

    Switching to pretrained model in hopefully simplified form.

--- examples/model_test.py
+++ examples/model_test.py
@@ -19,30 +19,19 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 #### /print debug information to stdout
 
 # # Read the dataset
-model_name = 'roberta-base'
+model_name = "output/training_agb_roberta-base-2020-03-28_11-25-57_og_consecutive_1"
 batch_size = 32
 agb_reader = AGBDataReader('datasets/AGB')
 train_num_labels = agb_reader.get_num_labels()
 
 
 # Use RoBERTa for mapping tokens to embeddings
-word_embedding_model = models.RoBERTa(model_name)
-
-# Apply mean pooling to get one fixed sized sentence vector
-pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-                               pooling_mode_mean_tokens=True,
-                               pooling_mode_cls_token=False,
-                               pooling_mode_max_tokens=False)
-
-model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+model = SentenceTransformer(model_name)
 
 train_loss = losses.SoftmaxLoss(model=model,
                                 sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
                                 num_labels=train_num_labels)
-#
 
-model_save_path = "output/training_agb_roberta-base-2020-03-28_11-25-57_og_consecutive_1"
-model = SentenceTransformer(model_save_path)
 test_data = SentencesDataset(examples=agb_reader.get_examples('test.tsv'), model=model, shorten=True)
 test_dataloader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
 evaluator = LabelAccuracyEvaluator(test_dataloader, softmax_model=train_loss)
