553ddecef6ae96da1f075f73a280d42384ef1db3
Dennis Aumiller <dennis.aumiller@gmx.de>
2020-04-08 11:17:07 +0200

    Creating version of eval script that re-trains the classification layer with frozen weights.

--- examples/model_test.py
+++ examples/model_test.py
@@ -9,6 +9,8 @@ from sentence_transformers import SentencesDataset, LoggingHandler, SentenceTran
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, LabelAccuracyEvaluator
 from sentence_transformers.readers import *
 import logging
+import torch
+import os
 from datetime import datetime
 
 #### Just some code to print debug information to stdout
@@ -19,7 +21,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 #### /print debug information to stdout
 
 # # Read the dataset
-model_name = "output/training_agb_roberta-base-2020-03-28_11-25-57_og_consecutive_1"
+model_name = "/data/daumiller/sentence-transformers/examples/output/training_agb_og_roberta-base-2020-03-26_10-53-54"
 batch_size = 32
 agb_reader = AGBDataReader('datasets/AGB')
 train_num_labels = agb_reader.get_num_labels()
@@ -28,10 +30,40 @@ train_num_labels = agb_reader.get_num_labels()
 # Use RoBERTa for mapping tokens to embeddings
 model = SentenceTransformer(model_name)
 
+# Don't train model, only train the loss layer, to retrieve results
+model.train(False)
+
+logging.info("Read AGB train dataset")
+train_data = SentencesDataset(agb_reader.get_examples('train.tsv'), model=model, shorten=True)
+train_dataloader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
 train_loss = losses.SoftmaxLoss(model=model,
                                 sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
                                 num_labels=train_num_labels)
 
+logging.info("Read AGB dev dataset")
+dev_data = SentencesDataset(examples=agb_reader.get_examples('dev.tsv'), model=model, shorten=True)
+dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=batch_size)
+evaluator = LabelAccuracyEvaluator(dev_dataloader, softmax_model=train_loss)
+
+# Configure the training
+num_epochs = 2
+
+warmup_steps = math.ceil(len(train_data) * num_epochs / batch_size * 0.1)  # 10% of train data for warm-up
+logging.info("Warmup-steps: {}".format(warmup_steps))
+
+model_save_path = 'output/retrain_'+model_name+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# Train the model
+model.fit(train_objectives=[(train_dataloader, train_loss)],
+          evaluator=evaluator,
+          epochs=num_epochs,
+          evaluation_steps=10000,
+          warmup_steps=warmup_steps,
+          output_path=model_save_path
+          )
+
+torch.save(train_loss, os.path.join(model_name, "classifier.pt"))
+
+logging.info("Read AGB test dataset")
 test_data = SentencesDataset(examples=agb_reader.get_examples('test.tsv'), model=model, shorten=True)
 test_dataloader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
 evaluator = LabelAccuracyEvaluator(test_dataloader, softmax_model=train_loss)
