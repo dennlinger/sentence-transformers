4df71febb79ba410184d1e21f39c45002b341ea0
Dennis Aumiller <dennis.aumiller@gmx.de>
2020-04-09 16:13:13 +0200

    First attempt of generating output with sentence-transformers.

--- examples/generate_predictions.py
+++ examples/generate_predictions.py
@@ -5,7 +5,7 @@ At every 1000 training steps, the model is evaluated on the AGB dev set.
 from torch.utils.data import DataLoader
 from sentence_transformers import models, losses
 from sentence_transformers import SentencesDataset, LoggingHandler, SentenceTransformer
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, LabelAccuracyEvaluator
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, LabelGenerationEvaluator
 from sentence_transformers.readers import *
 import logging
 import torch
@@ -18,7 +18,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 #### /print debug information to stdout    output2/run1/training_agb_roberta-base-2020-04-07_18-44-28_sections_1/
 
 # # Read the dataset
-model_save_path = "output2/run5/training_agb_bow-2020-04-08_23-10-14_og_consec_5"
+model_save_path = "/home/dennis/sentence-transformers/examples/output/training_agb_avg_word_embeddings-2020-03-27_17-23-51_og_consecutive_1"
 batch_size = 52
 agb_reader = AGBDataReader('datasets/AGB')
 train_num_labels = agb_reader.get_num_labels()
@@ -32,13 +32,8 @@ train_loss = losses.SoftmaxLoss(model=model,
                                 num_labels=train_num_labels)
 train_loss.classifier = torch.load(os.path.join(model_save_path, "2_Softmax/pytorch_model.bin"))
 
-print("dev")
-test_data = SentencesDataset(examples=agb_reader.get_examples('dev_raw.tsv'), model=model, shorten=True)
-test_dataloader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
-evaluator = LabelAccuracyEvaluator(test_dataloader, softmax_model=train_loss)
-model.evaluate(evaluator)
 print("test")
 test_data = SentencesDataset(examples=agb_reader.get_examples('test_raw.tsv'), model=model, shorten=True)
 test_dataloader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
-evaluator = LabelAccuracyEvaluator(test_dataloader, softmax_model=train_loss)
+evaluator = LabelGenerationEvaluator(test_dataloader, softmax_model=train_loss)
 model.evaluate(evaluator)
